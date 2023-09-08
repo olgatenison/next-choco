1d1d675dc729e6f6d2df3040a50c22b045687b22
olgatenison <88651744+olgatenison@users.noreply.github.com>
2023-09-08 16:47:53 +0300

    add  each product page

--- app/shop/[id]/page.js
+++ app/shop/[id]/page.js
@@ -1,3 +1,47 @@
+import "./id.css";
+
+import Image from "next/image";
+import Link from "next/link";
+var mydata = require("../../../data/products.json");
+
 export default function Page({ params }) {
-  return <div>Shop Item: {params.id}</div>;
+  var foundProduct = mydata.products.find((product) => product.id == params.id);
+
+  return (
+    <section className="product">
+      <div className="product__container container">
+        {/* Shop Item: {params.id}
+      {foundProduct.name} */}
+
+        <div className="product__item">
+          <div className="product__item-left">
+            <Image
+              src={foundProduct.imgSrc}
+              alt={foundProduct.name}
+              className="product__img"
+              width={260}
+              height={260}
+            />
+            <div className="product__item-down">
+              <div className="product__help">
+                <p className="product__price">{foundProduct.price}</p>
+                <p className="product__grm">{foundProduct.weight}</p>
+              </div>
+
+              <button className="product__btn btn">Buy</button>
+            </div>
+          </div>
+
+          <div className="product__item-right">
+            <h3 className="product__name">{foundProduct.name}</h3>
+            <p className="product__txt">{foundProduct.description}</p>
+            <p className="product__txt">{foundProduct.more}</p>
+            <Link href={"/shop/"} className="product__back">
+              Go back...
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
--- components/TheShop/ShopCard/ShopCard.js
+++ components/TheShop/ShopCard/ShopCard.js
@@ -1,19 +1,26 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const ShopCard = ({ product }) => {
-  const { imgSrc, name, description, price, weight } = product;
+  const { id, imgSrc, name, description, price, weight } = product;
   return (
     <li className="shop__item">
-      <Image
-        src={imgSrc}
-        alt={name}
-        className="shop__img"
-        width={260}
-        height={260}
-      />
-      <h3 className="shop__name">{name}</h3>
+      <Link href={"/shop/" + id}>
+        <Image
+          src={imgSrc}
+          alt={name}
+          className="shop__img"
+          width={260}
+          height={260}
+        />
+      </Link>
+      <h3 className="shop__name">
+        <Link href={"/shop/" + id}>{name}</Link>
+      </h3>
       <p className="shop__txt">{description}</p>
-
+      <Link href={"/shop/" + id} className="shop__more">
+        Read more...
+      </Link>
       <div className="shop__help">
         <p className="shop__price">{price}</p>
         <p className="shop__grm">{weight}</p>
