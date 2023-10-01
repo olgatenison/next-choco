05374e6d2edea4372a4fa2798ee3c4c11d01bf4d
olgatenison <88651744+olgatenison@users.noreply.github.com>
2023-10-01 15:48:56 +0300

    404

---
+++ pages/404.js
@@ -0,0 +1,20 @@
+// import Link from "next/link";
+
+// export default function ErrorPage() {
+//   return (
+//     <>
+//       <div>Error 404</div>
+//       <p>
+//         Please{" "}
+//         <link href="/">
+//           <a>go back</a>
+//         </link>{" "}
+//         to safty!
+//       </p>
+//     </>
+//   );
+// }
+
+export default function Custom404() {
+  return <h1>404 - Page Not Found</h1>;
+}
