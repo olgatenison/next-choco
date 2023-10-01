94966a20fac6252bf5e43bd12c858d3d3e2349ff
olgatenison <88651744+olgatenison@users.noreply.github.com>
2023-10-01 16:27:37 +0300

    404

--- pages/404.js
+++ pages/404.js
@@ -1,20 +1,60 @@
-// import Link from "next/link";
-
-// export default function ErrorPage() {
-//   return (
-//     <>
-//       <div>Error 404</div>
-//       <p>
-//         Please{" "}
-//         <link href="/">
-//           <a>go back</a>
-//         </link>{" "}
-//         to safty!
-//       </p>
-//     </>
-//   );
-// }
-
+import Link from "next/link";
+import "../app/globals.css";
 export default function Custom404() {
-  return <h1>404 - Page Not Found</h1>;
+  return (
+    <>
+      <Link
+        href="/"
+        className="header__logo logo"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div>
+          <h1>404 Page Not Found</h1>
+          <p>Please go back to maine</p>
+        </div>
+      </Link>
+
+      <style jsx>{`
+      div {
+          display: block;
+          position: absolute;
+          top: 40%;
+          left: 42%;
+          
+        } 
+         h1 {
+          color: #a0761a;
+                 
+          font-family: "Marcellus SC", serif;
+          font-weight: 400;
+          font-size: 24px;
+          line-height: 1.3;
+          letter-spacing: 2px;
+
+          padding: 8px 20px;
+          background-color: var(--bg-grey-color);
+          border: var(--txt-gold) solid 1px;
+
+          transition: all 0.3s ease 0s;
+
+        }
+        h1:hover,
+         h1:focus,
+        h1:active{
+          background-color:  #292929;
+          color: #fff;
+        }
+p{
+  color:#ffffff;
+  opacity:0.8;
+  margin-top:20px;
+  text-align:center;
+  
+} 
+
+        }
+      `}</style>
+    </>
+  );
 }
